55a61ea542a981ffd157c1e293850ad853dd4994
shubhamkanse72 <shubhamkanse7272@gmail.com>
2024-03-07 21:02:55 +0530

    lifting state up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import { useState } from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    intialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevValue) => {
+      return {
+        ...prevValue,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
     </>
   );
 }
